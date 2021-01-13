90ba9b095e2955adacadd2366b89c7ecd7191d29
moten-07 <58394845+moten-07@users.noreply.github.com>
2021-01-13 12:40:06 +0800

    moten

--- DemoA/app/src/main/java/com/example/redemo1/framents/HomeFragment.java
+++ DemoA/app/src/main/java/com/example/redemo1/framents/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.redemo1.framents;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -19,7 +20,7 @@ import com.example.redemo1.R;
  * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
     ViewPager viewPager;                        // 轮播图
     EditText seachstr;                          // 搜索框
@@ -81,5 +82,39 @@ public class HomeFragment extends Fragment {
         // 控件绑定
         viewPager = view.findViewById(R.id.viewpager);
         seachstr = view.findViewById(R.id.seach_str);
+        news_poins = new View[]{
+          view.findViewById(R.id.news_poin1),
+          view.findViewById(R.id.news_poin2),
+          view.findViewById(R.id.news_poin3)
+        };
+
+        btn_left = view.findViewById(R.id.btn_lastnew);
+        btn_right = view.findViewById(R.id.btn_nextnew);
+        btn_seach = view.findViewById(R.id.btn_seach);
+
+        lapplist = view.findViewById(R.id.lapp_list);
+        newslist = view.findViewById(R.id.news_list);
+
+        btn_left.setOnClickListener(this::onClick);
+        btn_seach.setOnClickListener(this::onClick);
+        btn_right.setOnClickListener(this::onClick);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_lastnew:
+                // 跳转到上一轮播图，首页则跳转到尾页
+                break;
+            case R.id.btn_nextnew:
+                // 跳转到下一轮播图，尾页则跳转至首页
+                break;
+            case R.id.seach_str:
+                String seach = seachstr.getText().toString();
+                // 获取输入内容，向服务器端查询
+                // startActivity(new Intent(v.getContext(),[跳转页面].class));
+                // 然后跳转到详情页
+                break;
+        }
     }
 }
